2d3989c98f37dc87a4da7a36864ebe5631d8d4d6
jun-tan <benjamintanjunyans@gmail.com>
2024-03-04 19:59:34 +0800

    Save untested solutions c01 ex00 - ex05

---
+++ ex03/ft_div_mod.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jun-tan <jun-tan@student.42singapore.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 17:32:30 by jun-tan           #+#    #+#             */
+/*   Updated: 2024/03/04 17:37:51 by jun-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_div_mod(int a, int b, int *div. int *mod)
+{
+	int result;
+	int remainder;
+
+	result = a / b;
+	remainder = a % b;
+	*div = result;
+	*mod = remainder;
+}
---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jun-tan <jun-tan@student.42singapore.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 17:39:19 by jun-tan           #+#    #+#             */
+/*   Updated: 2024/03/04 17:44:39 by jun-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_ultimate_div_mod(int *a, int *b)
+{
+	int num_a;
+	int num_b;
+	int result;
+	int remain;
+
+	num_a = *a;
+	num_b = *b;
+	result = num_a / num_b;
+	remain = num_a % num_b;
+
+	*a = result;
+	*b - remain;
+}
