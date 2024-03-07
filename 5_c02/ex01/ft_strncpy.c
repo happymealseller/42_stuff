3c9682aebecfbe00f37be99f1cb9e7e092285d46
jun-tan <benjamintanjunyans@gmail.com>
2024-03-07 21:38:44 +0800

    Format

--- 5_c02/ex01/ft_strncpy.c
+++ 5_c02/ex01/ft_strncpy.c
@@ -1,15 +1,24 @@
-char *ft_strncpy(char *dest, char *src, unsigned int n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jun-tan <jun-tan@student.42singapore.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 20:41:57 by jun-tan           #+#    #+#             */
+/*   Updated: 2024/03/07 20:45:47 by jun-tan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
-
-	while (i < n) {
+	while (i < n)
+	{
 		dest[i] = src[i];
 		i++;
 	}
-	
 	dest[i] = '\0';
-	return dest;
-
+	return (dest);
 }
